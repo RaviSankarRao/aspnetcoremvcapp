f4e5b2c95c12376421f3486bcc95d012bd3b3f6f
Ravi Sankar Rao <ravisankar139@gmail.com>
2024-05-24 23:42:48 -0700

    Added mvoies API controller

---
+++ Controllers/MoviesApiController.cs
@@ -0,0 +1,30 @@
+﻿using aspnetcoremvcapp.Models;
+using aspnetcoremvcapp.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetcoremvcapp.Controllers
+{
+    [Route("api/movies")]
+    [ApiController]
+    public class MoviesApiController : ControllerBase
+    {
+        private readonly IMoviesRepository _moviesRepository;
+
+        public MoviesApiController(IMoviesRepository moviesRepository)
+        {
+            _moviesRepository = moviesRepository;
+        }
+
+        [Route("")]
+        public async Task<List<Movie>> GetMovies()
+        {
+            return await _moviesRepository.GetMovies();
+        }
+
+        [Route("{id}")]
+        public async Task<Movie> GetMovieById([FromRoute]int? id)
+        {
+            return await _moviesRepository.GetMovieById(id);
+        }
+    }
+}
